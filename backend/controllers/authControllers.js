eae27758149b856f367da3fe865c22fe1120f27e
hariom <hk524569@gmail.com>
2024-03-16 21:42:22 +0530

    added function for singin page

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js'
 import {errorHandler} from '../utils/error.js'
-
+import bcryptjs from "bcryptjs";
+import jwttoken from 'jsonwebtoken';
 export const signup=async (req,res,next)=>{
    try{
     const {userName,email,password}=req.body
@@ -15,4 +16,26 @@ export const signup=async (req,res,next)=>{
     console.log('internal server error')
     next(err)
    }
+}
+
+export const signin=async (req,res,next)=>{
+    try{
+        const {email,password}=req.body
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(401).json({message:"user not found"})
+        }
+        const isMatch=bcryptjs.compareSync(password,user.password)
+        if(!isMatch){
+            return next(errorHandler(401,'Wrong credentials'))
+        }
+        const token=jwttoken.sign({id:user._id},process.env.JWT_SECRET)
+        const {password:pass,...rest}=user._doc
+        res.cookie('access_token',token,{httpOnly:true})
+        .status(200)
+        .json(rest)
+    }
+    catch(err){
+        next(err)
+    }
 }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signup } from '../controllers/authControllers.js'
+import { signup,signin } from '../controllers/authControllers.js'
 
 const router = express.Router()
 
 router.post('/signup',signup)
+router.post('/signin',signin)
 
 export default router
