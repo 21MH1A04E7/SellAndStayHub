1523f516c43f5f1e52ccbdd494533b39bdcd0a69
hariom <hk524569@gmail.com>
2024-03-18 22:46:30 +0530

    half half completed the firebase functionality

--- viteProject/src/Pages/ProfilePage.jsx
+++ viteProject/src/Pages/ProfilePage.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import {app} from '../firebase.js'
 import {useSelector} from 'react-redux'
+import { useRef } from 'react'
+import { set } from 'mongoose';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+
 function ProfilePage() {
+  const fileRef=useRef(null);
   const {currentUser}=useSelector((state)=>state.user)
+  const [file,setFile]=useState(undefined)
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file])
+  const handleFileUpload=(file)=>{
+        const storage=getStorage(app)
+        const fileName=new Date().getTime+file.name;//for unique file name
+        const storageRef=ref(storage,fileName)
+        const uploadTask=uploadBytesResumable(storageRef,file)
+        
+        uploadTask.on('state_changed',
+        (snapshot)=>{
+          const progress=((snapshot.bytesTransferred/snapshot.totalBytes)*100)
+          console.log('progress'+progress+'done')
+        },
+        )
+          
+  }
+  console.log(file)
   return (
     <div className='p-2 max-w-sm mx-auto'>
       <h2 className='text-3xl text-center font-bold my-5 italic hover:not-italic'>profile</h2>
       <form className='flex flex-col gap-2'>
-        <img src={currentUser.avatar} alt="profile" className='rounded-xl object-cover cursor-pointer w-22 h-22 self-center transition-transform hover:scale-110 ease-in-out shadow-md'/>
+        <input onChange={(e)=>setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image./*'/>
+        <img  src={currentUser.avatar} onClick={()=>fileRef.current.click()} alt="profile" className='rounded-xl object-cover cursor-pointer w-22 h-22 self-center transition-transform hover:scale-110 ease-in-out shadow-md'/>
         <input type='text' placeholder='userName' id='username' className='border  p-2 rounded-lg mt-2 bg-slate-100'/>
         <input type='email' placeholder='Email' id='email' className='border p-2 rounded-lg mt-2 bg-slate-100'/>
         <input type='password' placeholder='password' id='password' className='border p-2 rounded-lg mt-2 bg-slate-100'/>
@@ -21,3 +53,8 @@ function ProfilePage() {
 }
 
 export default ProfilePage
+
+// allow read;
+//       allow write: if
+//       request.resource.size < 2 * 1021 * 1024 &&
+//       request.resource.contentType.matches('image/.*')
