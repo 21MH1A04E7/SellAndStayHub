3495811a485fb2009610d999e72be808911b23b0
hariom <hk524569@gmail.com>
2024-03-23 22:15:51 +0530

    created listing api routers

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js"
+import Listing from "../models/listingModel.js"
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs'
 export const updateUser=async (req,res,next)=>{
@@ -30,5 +31,18 @@ export const deleteUser=async (req,res,next)=>{
         res.status(200).json('user has been deleted');
     }catch(error){
         next(error)
+    }
+}
+
+export const getUserListings=async (req,res,next)=>{
+    if(req.user.id===req.params.id){
+        try{
+            const listing=await Listing.find({userData:req.params.id});
+            res.status(200).json(listing);
+        }catch(error){
+    
+        }
+    }else{
+     return next(errorHandler(401,'you can not get other user lisging'))   
     }
 }
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {updateUser,deleteUser} from '../controllers/userControllers.js'
+import {updateUser,deleteUser,getUserListings} from '../controllers/userControllers.js'
 import {verifyToken} from '../utils/verifyToken.js'
 const router=express.Router();
 
 //checking the person is authenticated or not
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
+router.get('/listings/:id',verifyToken,getUserListings)
 
 export default router;
