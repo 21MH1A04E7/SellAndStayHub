52a6e1930cdba4ca315500ef31842a2bd9491354
hariom <hk524569@gmail.com>
2024-03-23 20:59:39 +0530

    added new singup pages

--- viteProject/src/Pages/SignUp.jsx
+++ viteProject/src/Pages/SignUp.jsx
@@ -40,48 +40,141 @@ function SignUp() {
   };
   console.log(formData);
   return (
-    <div className="max-w-sm mx-auto p-3">
-      <h1 className="text-2xl text-center font-semibold my-7 ms:text-3xl font-serif">
-        SignUp
-      </h1>
-      <form className="flex flex-col gap-2 bg-slate" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="User Name"
-          id="userName"
-          className="rounded-lg bg-slate-200 p-2 px-8 focus:outline-none"
-          onChange={handleChange}
+    <section className="p-1 sm:m-auto sm:p-4">
+  <div className="grid grid-cols-1 lg:grid-cols-2">
+    <div className="relative flex items-end px-4 pb-10 pt-60 sm:px-6 sm:pb-16 md:justify-center lg:px-8 lg:pb-24">
+      <div className="absolute inset-0">
+        <img
+          className="h-full w-full rounded-md object-cover object-top"
+          src="https://st2.depositphotos.com/1637787/6885/i/450/depositphotos_68857205-stock-photo-couple-holding-a-minature-house.jpg"
+          alt="home"
         />
-        <input
-          type="email"
-          placeholder="User Email"
-          id="email"
-          className="rounded-lg bg-slate-200 p-2 px-8 focus:outline-none"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          id="password"
-          className="rounded-lg bg-slate-200 p-2 px-8 focus:outline-none"
-          onChange={handleChange}
-        />
-        <button
-          disabled={loading}
-          className="uppercase bg-[#30336b] rounded-lg p-2 hover:opacity-90 disabled:opacity-80 text-white"
-        >
-          {loading ? "Loading..." : "SignUp"}
-        </button>
-        <OAuth />
-      </form>
-      <div className="flex gap-3 py-2">
-        <p>Have an account?</p>
-        <Link to="/sign-in">
-          <span className="text-blue-600">SignIn</span>
-        </Link>
       </div>
-      {error&&<p className="text-red-600">something is wrong</p>}
+      <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
+      <div className="relative">
+        <div className="w-full max-w-xl xl:mx-auto xl:w-full xl:max-w-xl xl:pr-24">
+          <h3 className="text-4xl font-bold text-white">
+            wellcome to stayandsell site...
+          </h3>
+        </div>
+      </div>
     </div>
+    <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+      <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+      {error&&<p className="text-red-600">something is wrong</p>}
+        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">
+          Sign up
+        </h2>
+        <p className="mt-2 text-base text-gray-600">
+          Already have an account?
+          <Link
+           to="/sign-in"
+            className="font-medium text-blue-400 transition-all duration-200 hover:underline"
+          >
+            Sign In
+          </Link>
+        </p>
+        <form className="mt-8" onSubmit={handleSubmit}>
+          <div className="space-y-5">
+            <div>
+              <label htmlFor="userName" className="text-base font-medium text-gray-900">
+                
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  type="text"
+                  placeholder="User Name"
+                  id="userName"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="text-base font-medium text-gray-900">
+                
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  type="email"
+                  placeholder="Email"
+                  id="email"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="text-base font-medium text-gray-900"
+                >
+                  
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                disabled={loading}
+                className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+              >
+                {loading?"Loading....":"Create Account"}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="ml-2"
+                >
+                  <line x1="5" y1="12" x2="19" y2="12"></line>
+                  <polyline points="12 5 19 12 12 19"></polyline>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="mt-3 space-y-3">
+          <OAuth/>
+          <button
+            type="button"
+            className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+          >
+            <span className="mr-2 inline-block">
+              <svg
+                className="h-6 w-6 text-[#2563EB]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
+              </svg>
+            </span>
+            Sign up with Facebook
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
   );
 }
 
