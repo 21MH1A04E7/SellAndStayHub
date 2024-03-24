ce323eeb441624f4b7fd8d53b0b28e4aa4afc984
hariom <hk524569@gmail.com>
2024-03-24 11:28:03 +0530

    update pai routes createred

--- backend/controllers/listingControllers.js
+++ backend/controllers/listingControllers.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listingModel.js'
+import { errorHandler } from '../utils/error.js';
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -24,3 +25,20 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing=async (req, res, next) => {
+  try{
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404,'Room not found'));
+    }
+     // Check if the user is the owner of the listing
+     if (listing.userData !== req.user.id) {
+      return next(errorHandler(401,'You are not authorized to perform this action'));
+    }
+    const response=await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    res.status(200).json(response)
+  }catch(error){
+    next(error);
+  }
+}
--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createListing,deleteListing} from '../controllers/listingControllers.js';
+import { createListing,deleteListing,updateListing} from '../controllers/listingControllers.js';
 import {verifyToken} from '../utils/verifyToken.js'
 const router=express.Router();
 
 router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteListing)
+router.post('/update/:id',verifyToken,updateListing)
 
 export default router;
