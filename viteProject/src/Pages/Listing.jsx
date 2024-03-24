1357b5bf322393eeb93c0a90b501c1570d4fc3e7
hariom <hk524569@gmail.com>
2024-03-24 16:26:58 +0530

    working on listing page

--- viteProject/src/Pages/Listing.jsx
+++ viteProject/src/Pages/Listing.jsx
@@ -1,63 +1,114 @@
-import { useEffect,useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 //npm i swiper
-import {Swiper,SwiperSlide} from 'swiper/react'
-import SwiperCore from 'swiper'
-import {Navigation} from 'swiper/modules'
-import 'swiper/css/bundle'
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
 function Listing() {
-    SwiperCore.use([Navigation])
-    const params=useParams()
-    const [listing,setListing]=useState(null)
-    const [error,setError]=useState(false)
-    const [loading,setLoading]=useState(false)
-    console.log(listing)
-    useEffect(() => {
-        const fetchListing = async () => {
-          try {
-            setLoading(true);
-            const res = await fetch(`/api/listing/getListing/${params.listingId}`);
-            const data = await res.json();
-            if (data.success === false) {
-              setError(true);
-              setLoading(false);
-              return;
-            }
-            setListing(data);
-            setLoading(false);
-            setError(false);
-          } catch (error) {
-            setError(true);
-            setLoading(false);
-          }
-        };
-        fetchListing();
-      }, [params.listingId]);
+  SwiperCore.use([Navigation]);
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  console.log(listing);
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/getListing/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
   return (
     <main>
-        {loading&&<p className="text-green-600 text-center text-lg font-semibold my-5">Loading....</p>}
-        {error&&<p className="text-red-500 text-center text-lg font-semibold my-5">Someting is wrong....</p>}
-        {listing && !loading && !error && (
-            <div>
-                 <Swiper navigation>
+      {loading && (
+        <p className="text-green-600 text-center text-lg font-semibold my-5">
+          Loading....
+        </p>
+      )}
+      {error && (
+        <p className="text-red-500 text-center text-lg font-semibold my-5">
+          Someting is wrong....
+        </p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
             {listing.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className='h-[580px]'
+                  className="h-[580px]"
                   style={{
                     background: `url(${url}) center no-repeat`,
-                    backgroundSize: 'cover',
+                    backgroundSize: "cover",
                   }}
                 ></div>
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
+            <p className="text-2xl font-semibold">
+              {listing.name} - ${" "}
+              {listing.offer
+                ? listing.discountPrice.toLocaleString("en-US")
+                : listing.regularPrice.toLocaleString("en-US")}
+              {listing.type === "rent" && " / month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {listing.address}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                </p>
+              )}
             </div>
-        )}
-        {/* <p>{listing.name}</p> */}
+          </div>
+        </div>
+      )}
+      {/* <p>{listing.name}</p> */}
     </main>
-  )
+  );
 }
 
-export default Listing
+export default Listing;
