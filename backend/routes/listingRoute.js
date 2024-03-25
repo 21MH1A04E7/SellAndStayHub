fdef588858630c537177e5448c00bf41a8be49ed
hariom <hk524569@gmail.com>
2024-03-25 17:47:59 +0530

    creaed

--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing,deleteListing,updateListing,getListing} from '../controllers/listingControllers.js';
+import { createListing,deleteListing,updateListing,getListing,getListingAll} from '../controllers/listingControllers.js';
 import {verifyToken} from '../utils/verifyToken.js'
 const router=express.Router();
 
@@ -7,5 +7,6 @@ router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteListing)
 router.post('/update/:id',verifyToken,updateListing)
 router.get('/getListing/:id',getListing);
+router.get('/getListing',getListingAll);
 
 export default router;
