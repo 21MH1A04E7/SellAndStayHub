49a7418802169cd3f8f381edfadd2fb271634e5e
hariom <hk524569@gmail.com>
2024-04-26 21:18:53 +0530

    add build in scrept

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/userRouter.js';
 import authRouter from './routes/authRoute.js';
 import cookieParser from 'cookie-parser'
 import listingRouter from './routes/listingRoute.js'
+import path from 'path';
 dotenv.config()
 const app=express()
 
@@ -16,6 +17,8 @@ connecteMongoDb(process.env.Mongo)
 .catch((err)=>{
     console.log(`error ${err}`)
 })
+
+const __dirname=path.resolve();
 
 //middleWare
 app.use(express.json())
@@ -30,6 +33,11 @@ app.listen(8080,()=>{
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join( __dirname,'/viteProject/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join( __dirname,'viteProject','dist','index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
